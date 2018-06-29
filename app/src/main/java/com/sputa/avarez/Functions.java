af2bb26ba6e7226844606a64601a72ba1b12d611
majid alamdary <majidalamdary@gmail.com>
2018-06-29 15:31:50 +0430

    Initial commit

---
+++ app/src/main/java/com/sputa/avarez/Functions.java
@@ -0,0 +1,71 @@
+package com.sputa.avarez;
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mma on 5/5/2018.
+ */
+
+public class Functions {
+
+
+    public static String[] arr_item_status = new String[] { "همه",
+            "در انتظار تائید", "تائید شده", "ویرایش شده", "حذف شده"
+    };
+    public String
+            font_name_pens = "fonts/pens.ttf";
+    public String
+            font_name_Byekan = "fonts/BYekan.ttf";
+    public String
+            font_name_yekan = "fonts/Yekan.ttf";
+    public static String
+            font_name_iiranSens = "fonts/iransans.ttf";
+
+
+    public static String u_id = "";
+    public static String u_name = "";
+
+    public static int    user_normal =1;
+    public Functions() {
+
+    }
+
+    public  void enableDisableView(View view, boolean enabled) {
+        view.setEnabled(enabled);
+        if ( view instanceof ViewGroup) {
+            ViewGroup group = (ViewGroup)view;
+
+            for ( int idx = 0 ; idx < group.getChildCount() ; idx++ ) {
+                enableDisableView(group.getChildAt(idx), enabled);
+            }
+        }
+    }
+
+    public Typeface get_font_pens(AssetManager context)
+    {
+        Typeface tf_items =  Typeface.createFromAsset(context,font_name_pens );
+        return tf_items;
+    }
+    public  Typeface get_font_byekan(AssetManager context)
+    {
+        Typeface tf_items =  Typeface.createFromAsset(context,font_name_Byekan );
+        return tf_items;
+    }
+    public  Typeface get_font_yekan(AssetManager context)
+    {
+        Typeface tf_items =  Typeface.createFromAsset(context,font_name_yekan );
+        return tf_items;
+    }
+    public static   Typeface get_font_iransens(AssetManager context)
+    {
+        Typeface tf_items =  Typeface.createFromAsset(context,font_name_iiranSens );
+        return tf_items;
+    }
+
+}
+
+
+
---
+++ app/src/main/java/com/sputa/avarez/MainActivity.java
@@ -0,0 +1,88 @@
+package com.sputa.avarez;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int screenWidth;
+    private int screenHeight;
+    private void set_size(int vid,Double width,Double height,String typ)
+    {
+        View v =findViewById(vid);
+        if(typ.equals("cons")) {
+            ConstraintLayout.LayoutParams lp= (ConstraintLayout.LayoutParams) v.getLayoutParams();
+            lp.width=(int)(screenWidth* width);
+            lp.height=(int)(screenHeight* height);;
+            v.setLayoutParams(lp);
+        }
+        if(typ.equals("line")) {
+            LinearLayout.LayoutParams lp= (LinearLayout.LayoutParams) v.getLayoutParams();
+            lp.width=(int)(screenWidth* width);
+            lp.height=(int)(screenHeight* height);;
+            v.setLayoutParams(lp);
+        }
+        if(typ.equals("rel")) {
+            RelativeLayout.LayoutParams lp= (RelativeLayout.LayoutParams) v.getLayoutParams();
+            lp.width=(int)(screenWidth* width);
+            lp.height=(int)(screenHeight* height);;
+            v.setLayoutParams(lp);
+        }
+    }
+    private void set_size_txt(int vid,Double size,String typ)
+    {
+        TextView v =(TextView) findViewById(vid);
+        if(typ.equals("cons")) {
+            ConstraintLayout.LayoutParams lp= (ConstraintLayout.LayoutParams) v.getLayoutParams();
+            v.setTextSize(TypedValue.COMPLEX_UNIT_PX, (int) (screenWidth * size));
+            v.setLayoutParams(lp);
+        }
+        if(typ.equals("line")) {
+            LinearLayout.LayoutParams lp= (LinearLayout.LayoutParams) v.getLayoutParams();
+            v.setTextSize(TypedValue.COMPLEX_UNIT_PX, (int) (screenWidth * size));
+            v.setLayoutParams(lp);
+        }
+        if(typ.equals("rel")) {
+            RelativeLayout.LayoutParams lp= (RelativeLayout.LayoutParams) v.getLayoutParams();
+            v.setTextSize(TypedValue.COMPLEX_UNIT_PX, (int) (screenWidth * size));
+            v.setLayoutParams(lp);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE); // the results will be higher than using the activity context object or the getWindowManager() shortcut
+        wm.getDefaultDisplay().getMetrics(displayMetrics);
+        screenWidth = displayMetrics.widthPixels;
+        screenHeight = displayMetrics.heightPixels;
+
+        set_size(R.id.btn_search,.67,.12,"cons");
+        set_size(R.id.btn_peygiri,.67,.125,"cons");
+        set_size(R.id.btn_setting,.67,.125,"cons");
+        set_size(R.id.btn_exit,.67,.125,"cons");
+
+        set_size_txt(R.id.lbl_subject,.055,"line");
+        set_size_txt(R.id.lbl_peygiri,.055,"line");
+        set_size_txt(R.id.lbl_setting,.055,"line");
+        set_size_txt(R.id.lbl_exit,.055,"line");
+        set_size_txt(R.id.lbl_title,.08,"cons");
+
+
+    }
+
+    public void clk_search(View view) {
+        Intent I = new Intent(this,CarSearch.class);
+        startActivity(I);
+    }
+}
